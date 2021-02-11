82d32318bad386b241f54f40e000a1638c05f761
Sakshat Rao <sakshatrao99@gmail.com>
2021-02-11 15:08:02 +0530

    Removed django-heroku for PythonAnywhere

--- SmartHospitalServer/settings.py
+++ SmartHospitalServer/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -135,5 +134,3 @@ STATIC_TMP = os.path.join(BASE_DIR, 'static')
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
